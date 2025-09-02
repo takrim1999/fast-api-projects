dd2a19ed1fb7078ef2e76f1f880635f8f5c591ec
takrim1999 <takrimulalom@gmail.com>
2025-09-02 23:51:54 +0600

    OOP game complete

--- basic-revision/oop-project/enemy.py
+++ basic-revision/oop-project/enemy.py
@@ -1,8 +1,13 @@
+import random
+
 class Enemy:
-    def __init__(self, type_of_enemy):
+    def __init__(self, type_of_enemy, health_point, attack_point):
         self.__type_of_enemy:str = type_of_enemy
-        self.health_points:int = 10
-        self.attack_points:int = 1
+        self.health_points:int = health_point
+        self.attack_points:int =attack_point
+
+    def get_enemy_type(self):
+        return self.__type_of_enemy
 
     def talk(self):
         print(f"""
@@ -11,11 +16,14 @@ class Enemy:
         And I can attack at {self.attack_points} 
         """)
 
+    def attack(self):
+        print(f"{self.__type_of_enemy} is attacking for {self.attack_points}")
+
 
 class Zombie(Enemy):
 
     def __init__(self):
-        super().__init__("Zombie")
+        super().__init__("Zombie", 10, 1)
 
     def talk(self):
         print("Grambling")
@@ -23,15 +31,22 @@ class Zombie(Enemy):
     def spreading_infection(self):
         print("Spreading some infections")
 
-
+    def special_ability(self):
+        if random.random()<0.5:
+            self.health_points += 2
+            print("Zombie Health incresed by 2!")
 class Ogre(Enemy):
 
     def __init__(self):
-        super().__init__("Ogre")
+        super().__init__("Ogre", 20, 3)
 
     def talk(self):
         print("Orge Comming, Smashing everything!")
 
+    def special_ability(self):
+        if random.random()<0.2:
+            self.attack_points += 4
+            print("Ogre attack incresed by 4!")
 
 # if __name__ == "__main__":
 #     test = Enemy()
--- basic-revision/oop-project/main.py
+++ basic-revision/oop-project/main.py
@@ -1,6 +1,27 @@
-from enemy import Zombie, Ogre
+from enemy import Zombie, Ogre, Enemy
+
+
+def battle(e1: Enemy, e2: Enemy):
+    e1.talk()
+    e2.talk()
+
+    while e1.health_points>0 and e2.health_points>0:
+        print("-------------")
+        e1.special_ability()
+        e2.special_ability()
+        print(f"{e1.get_enemy_type()} has {e1.health_points} HP left")
+        print(f"{e2.get_enemy_type()} has {e2.health_points} HP left")
+        e1.attack()
+        e2.health_points -= e1.attack_points
+        e2.attack()
+        e1.health_points -= e2.attack_points
+    if e1.health_points > 0:
+        print(f"{e1.get_enemy_type()} Won!")
+    else:
+        print(f"{e2.get_enemy_type()} Won!")
 
 enemy1 = Zombie()
-enemy1.talk()
 enemy2 = Ogre()
-enemy2.talk()
+
+
+battle(enemy1,enemy2)
